11de223f6801a8aaa4a8922cd0a3eb535228f3e0
Vemas7731 <134183549+Vemas7731@users.noreply.github.com>
2025-02-27 20:55:38 +0700

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -3,7 +3,7 @@ pipeline {
 
     environment {
         DISCORD_WEBHOOK_URL = 'https://discordapp.com/api/webhooks/1344552070253908008/cb713-OKHK1-h0ReOPTp97mbbC1X4Tlsxj52c4F0knz7LJD0FslDoDuSmb6_NAlmomxG'
-        SONARQUBE_SERVER = 'SonarQube'  // Nama credential SonarQube di Jenkins
+        SONARQUBE_SERVER = 'sonarqube-server'  // Nama credential SonarQube di Jenkins
     }
 
     stages {
