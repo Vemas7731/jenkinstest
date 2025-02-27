6678921c064aa9bd10e3f3ccad5a3461c606b015
Vemas7731 <134183549+Vemas7731@users.noreply.github.com>
2025-02-27 21:43:55 +0700

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -3,7 +3,7 @@ pipeline {
 
     environment {
         DISCORD_WEBHOOK_URL = 'https://discordapp.com/api/webhooks/1344552070253908008/cb713-OKHK1-h0ReOPTp97mbbC1X4Tlsxj52c4F0knz7LJD0FslDoDuSmb6_NAlmomxG'
-        SONARQUBE_SERVER = 'sonar-token'  // Nama credential SonarQube di Jenkins
+        SCANNER_HOME = tool 'sonar-scanner-7' // Sesuaikan dengan nama scanner
     }
 
     stages {
@@ -25,7 +25,7 @@ pipeline {
         stage('SonarQube Analysis') {
             steps {
                 script {
-                    withSonarQubeEnv('sonar-scanner') {  // Sesuaikan dengan nama server di Jenkins
+                    withSonarQubeEnv('sonarqube-server') {  // Sesuaikan dengan nama server di Jenkins
                         sh 'sonar-scanner -Dsonar.projectKey=jenkinstest -Dsonar.sources=.'
                     }
                 }
